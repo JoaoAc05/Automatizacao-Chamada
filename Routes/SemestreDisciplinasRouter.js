c4510c71137382402ea4dd859502e759fe440636
João Alves <joaoalvescordeiro2005@gmail.com>
2025-04-11 12:59:49 -0400

    Dsiciplina professor

--- Routes/SemestreDisciplinasRouter.js
+++ Routes/SemestreDisciplinasRouter.js
@@ -6,8 +6,8 @@ const SemestreDisciplinasRouter = express.Router();
 const SemestreDisciplinasController = new semestreDisciplinasController();
 
 SemestreDisciplinasRouter.get('/professor/:id_professor', auth, permissao([1, 2]), SemestreDisciplinasController.disciplinaProfessor);
-SemestreDisciplinasRouter.get('/:id_semestre/disciplinas', permissao([2]), auth, SemestreDisciplinasController.getId); // ID DO SEMESTRE DO ANO
-SemestreDisciplinasRouter.get('/disciplinas', permissao([2]), auth, SemestreDisciplinasController.getAll); 
+SemestreDisciplinasRouter.get('/:id_semestre/disciplinas',  auth, permissao([2]), SemestreDisciplinasController.getId); // ID DO SEMESTRE DO ANO
+SemestreDisciplinasRouter.get('/disciplinas', auth, permissao([2]), SemestreDisciplinasController.getAll); 
 SemestreDisciplinasRouter.post('/disciplinas', auth, permissao([2]), SemestreDisciplinasController.cadastro); 
 SemestreDisciplinasRouter.put('/disciplinas', auth, permissao([2]), SemestreDisciplinasController.alterar); 
 SemestreDisciplinasRouter.delete('/disciplinas/:id', auth, permissao([2]), SemestreDisciplinasController.deletar); // ID PROPRIO
