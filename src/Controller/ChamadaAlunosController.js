a4f3ce86b3d9c270491ab1e6f4630d5828aab317
João Alves <joaoalvescordeiro2005@gmail.com>
2025-06-18 20:37:00 -0400

    observacao

--- src/Controller/ChamadaAlunosController.js
+++ src/Controller/ChamadaAlunosController.js
@@ -371,8 +371,11 @@ class chamadaAlunosController {
     }
     
     async deletar(req, res) { // As presenças não podem ser excluidas de forma alguma, então será dado apenas o update no status
-        const { id_chamada, id_aluno, id_vinculo } = req.query;
-        // const { observacao } = req.query;
+        const { id_chamada, id_aluno, id_vinculo, observacao } = req.query;
+
+        if (!observacao || observacao.length < 10) {
+            return res.status(400).json({ message: 'Favor informar uma observação com mais de 10 caracteres.'})
+        }
 
         if (id_vinculo) {
             const chamadaAluno = await prisma.chamadaAlunos.findUnique({
@@ -389,8 +392,8 @@ class chamadaAlunosController {
                     id: Number(id_vinculo)
                 },
                 data: {
-                    status: 0 // Presença Removida
-                    //,observacao: observacao
+                    status: 0, // Presença Removida
+                    observacao: observacao
                 }
             })
             if (!deletePresenca) {
