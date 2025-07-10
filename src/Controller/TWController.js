f5e2d25f2a39b5ac641746fd24fac758bf0cbae5
João Alves <joaoalvescordeiro2005@gmail.com>
2025-07-10 19:28:24 -0400

    time

--- src/Controller/TWController.js
+++ src/Controller/TWController.js
@@ -3,13 +3,13 @@ import axios from "axios";
 class twController {
     async tempo(req, res) {
         try {
-            const response = await axios.get("https://worldtimeapi.io/api/timezone/America/Cuiaba");
+            const response = await axios.get("https://timeapi.io/api/Time/current/zone?timeZone=America/Cuiaba");
             const data = response.data;
 
-            res.setHeader("Access-Control-Allow-Origin", "*"); // para permitir uso via navegador
-            res.status(200).json({ datetime: data.datetime });
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.status(200).json({ datetime: data.dateTime }); // usa dateTime da nova API
         } catch (error) {
-            console.error("Erro ao obter horário:", error.message); // importante para depuração
+            console.error("Erro ao obter horário:", error.message);
             res.status(500).json({ error: "Erro ao obter o horário." });
         }
     }
